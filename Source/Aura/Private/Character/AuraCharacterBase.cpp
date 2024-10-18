f0994bdf083f0cd9e8167c6e6a2a828c5163f03b
yuyang <yushaoy2011@163.com>
2024-10-19 02:27:23 +0800

    Character Blueprint Setup

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -6,15 +6,21 @@
 // Sets default values
 AAuraCharacterBase::AAuraCharacterBase()
 {
- 	// 设置为false，无需每帧执行Tick函数
+	// 设置为false，无需每帧执行Tick函数
 	PrimaryActorTick.bCanEverTick = false;
 
+	//创建Weapon组件，传参数为组件名称，类型FName,FName类型无需使用TEXT宏，直接输入字符即可，可自动转换
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	//将武器附加在名称为"WeaponHandSocket"的插座上
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	//武器没有碰撞
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AAuraCharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 
