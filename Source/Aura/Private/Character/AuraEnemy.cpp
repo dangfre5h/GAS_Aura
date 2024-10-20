ff36a8ec4a1e9d8c7b6a2b95cbe1fe5ffdecfa6d
yuyang <yushaoy2011@163.com>
2024-10-20 19:47:01 +0800

    Post Process Highlight

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -3,12 +3,32 @@
 
 #include "Character/AuraEnemy.h"
 
+#include "Aura/Aura.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+	//设置成block才能被鼠标指向识别
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+}
+
 void AAuraEnemy::HighlightActor()
 {
-	HitSomthing = true;
+	//因为后期处理体积PostProcessVolume下面所选择的材料，是针对CustomDepth为250的网格进行高亮，所以此处赋值250
+	GetMesh()->SetRenderCustomDepth(true);
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+
+	if (Weapon)
+	{
+		Weapon->SetRenderCustomDepth(true);
+		Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	}
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-	HitSomthing = false;
+	GetMesh()->SetRenderCustomDepth(false);
+	if (Weapon)
+	{
+		Weapon->SetRenderCustomDepth(false);
+	}
 }
